d9dcdaf98fd033e487370d6c6894765759e37e62
Ricky Buchanan <rb@tertius.net.au>
1998-09-06 11:26:28 +0000

    Import of GOSSAMER/Text-LineEditor-0.03 from CPAN.
    gitpan-cpan-distribution: Text-LineEditor
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         GOSSAMER/Text-LineEditor-0.03.tar.gz
    gitpan-cpan-author:       GOSSAMER
    gitpan-cpan-maturity:     released

--- LineEditor.pm
+++ LineEditor.pm
@@ -5,7 +5,7 @@ package Text::LineEditor;
 #    free software; you can redistribute it and/or modify it under the
 #    same terms as Perl itself.
 #
-# Last updated by gossamer on Tue Aug 25 18:52:18 EST 1998
+# Last updated by gossamer on Sun Sep  6 21:03:23 EST 1998
 #
 
 use strict;
@@ -19,7 +19,7 @@ use Term::ReadLine;
 @EXPORT = qw( line_editor );
 @EXPORT_OK = qw();
 
-$VERSION = "0.02";
+$VERSION = "0.03";
 
 #
 # Constants
@@ -42,9 +42,45 @@ my $Tempfile_Name = "/tmp/lineeditor.$$";
 #
 
 sub find_editor {
-   return $ENV{"visual"} || "vim" || "vi";
+   return $ENV{"VISUAL"} || $ENV{"EDITOR"} || "vim" || "vi" || "pico";
 }
 
+=head1 NAME
+
+Text::LineEditor - simple line  editor
+
+=head1 SYNOPSIS
+
+   use Text::LineEditor;
+
+   $text = &line_editor();
+
+=head1 DESCRIPTION
+
+C<Text::LineEditor> implements a -very- simple editor like Berkley
+mail used to use.
+
+=head1 FUNCTIONS
+
+=item line_editor ();
+
+Returns the text entered.  If the text has been abandoned with
+~x it returns an empty string.
+
+=head1 EDITING COMMANDS
+
+To append text, just type.
+To end, type '.' by itself on a line.
+Tilde commands for editing (by themselves on a line):
+   ~h     This help message
+   ~e, ~v Edit message using visual editor
+   ~w <filename>
+          Write current text to file <filename>
+   ~p     Print current text using 'lpr'
+   ~.     End message as normal
+   ~x     Quit, abandon text
+
+=cut
 sub line_editor {
    my $text;
 
@@ -64,11 +100,34 @@ sub line_editor {
       } elsif ($line =~ m/^~(.)(.*)$/) {
          # Something magic
 
-         if ($1 eq 'w') {
+         if ($1 eq 'h') {
+            # give help
+            print <<"EOT";
+To append text, just type.
+To end, type '.' by itself on a line.
+Tilde commands for editing (by themselves on a line):
+   ~h     This help message
+   ~e, ~v Edit message using visual editor
+   ~w <filename>
+          Write current text to file <filename>
+   ~p     Print current text using 'lpr'
+   ~.     End message as normal
+   ~x     Quit, abandon text
+EOT
+         } elsif ($1 eq '.') {
+            # same as regular ending
+            $finished++;
+         } elsif ($1 eq 'w') {
             # write to a file
             open(OUTFILE, ">$2");
             print OUTFILE $text;
             close(OUTFILE);
+         } elsif ($1 eq 'p') {
+            # print
+            open PRINTER, "| lpr" ||
+               warn "Can't open printer: $!\n"; 
+            print PRINTER $text; 
+            close PRINTER;
          } elsif (($1 eq 'e') || ($1 eq 'v')) {
             # visual editor
             my $editor = &find_editor;
@@ -84,11 +143,14 @@ sub line_editor {
                $text = <OUTFILE>;
                close(OUTFILE);
 
-               print "DEBUG:  '$text'\n";
+               #print "DEBUG:  '$text'\n";
             } else {
                print $Message_Prompt . "Can't find an editor to use!\n";
             }
 
+         } elsif ($1 eq 'x') {
+            $finished++;
+            $text = "";
          } else {
             print $Message_Prompt . "Unknown tilde escape '$line'\n";
          }
